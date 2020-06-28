7b22b0be3a17d14907b9f1419f4393559b8de6e4
artyom <you@example.com>
2020-06-28 16:26:58 +0300

    refaq

---
+++ utils.c
@@ -0,0 +1,35 @@
+#include "parse.h"
+
+void	show_byte(unsigned char byte, t_vm *vm)
+{
+	char *sym;
+
+	sym = ft_itoa_base(byte, 16);
+	ft_putstr(sym);
+	free(sym);
+}
+
+int		read_byte(int fd, unsigned char *byte)
+{
+	if (read(fd, &(*byte), 1) > 0)
+		return (0);
+	else
+		return (-1);
+}
+
+void	nthng(unsigned char byte, t_vm *vm)
+{
+}
+
+int 	read_nbytes(t_vm *vm, int nbytes, void (*f)(unsigned char, t_vm *vm))
+{
+	int				i;
+	unsigned char	byte;
+	t_file_info		*player;
+
+	i = -1;
+	player = vm->current;
+	while (++i < nbytes && read_byte(player->fd, &byte) == 0)
+		f(byte, vm);
+	return (i);
+}
