b123f4e373a2e036b80d4fce2c2b1f60e2f05218
orsk-moscow <moscow.orsk@gmail.com>
2020-07-04 23:04:09 +0300

    checker ready

--- README.md
+++ README.md
@@ -1,9 +1,27 @@
 # VM
 
-## How to use  
-`make`  
-`./vm 42.cor`  
-and displey magic header  
+## Checker usage
+
+`$ python3 checker.py --start <number-of-cicles-to-start-comparing> --end <number-of-cicles-before-game-will-stopped> <path/to/.cor/file/N1> <path/to/.cor/file/N2> ...`
+
+Example:
+
+`$ python3 checker.py --start 0 --end 10 mac-os-vm-champ/batman.cor mac-os-vm-champ/robin.cor ...`
+`$ python3 checker.py mac-os-vm-champ/batman.cor --start 95 --end 100 mac-os-vm-champ/robin.cor ...`
+`$ python3 checker.py mac-os-vm-champ/batman.cor mac-os-vm-champ/robin.cor ...`
+
+`--end` flag responsible for number of cicles. After execution this number of cicles game will be stopped and memory will be dumped into stdout.
+If `--end` not given, it will be placed to 10 cicles.
+
+`--start` flag responsible for number of cicles when checker start to compare output your's and default corewars.
+If `--start` not given, it will be placed to 10 cicles.
+
+Checker will recompile project by his own, you sholdn't do anything else.
+
+## How to use
+`make`
+`./vm 42.cor`
+and displey magic header
 `0ea83f3`
 
 ## TODO
--- checker.py
+++ checker.py
@@ -1,101 +1,30 @@
-# To add a new cell, type '# %%'
-# To add a new markdown cell, type '# %% [markdown]'
-# %%
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 
 # 0. Libraries import
 import warnings
 warnings.filterwarnings(action='ignore')
-
 import logging
 import subprocess
+import os
+import sys
+
+# 1. Local variables and function definition
+CEND    = '\33[0m'
+CRED    = '\33[31m'
+CGREEN  = '\33[32m'
+
 logging.getLogger().setLevel(logging.INFO)
 
 def run_cmd(cmd):
-    logging.getLogger().setLevel(logging.INFO)
     logging.info("%s"%cmd)
     cmd = cmd.split()
     p = subprocess.run(cmd,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE)
-    # if p.stdout: logging.info("stdout\n%s"%bytes.decode(p.stdout))
     if p.stderr: logging.warn("stderr\n%s"%bytes.decode(p.stderr))
     return bytes.decode(p.stdout)
 
-import os
-
-try:import numpy
-except:os.system("pip3 install -q numpy --user")
-import io
-import sys
-
-logging.info("%s"%(sys.platform))
-
-
-# %%
-from colors import *
-
-
-# %%
-if 'Makefile' in set(os.listdir('.')):
-    logging.info("will call a \'make\' command")
-    run_cmd("make re")
-
-
-# %%
-exe_corewar_sys = './mac-os-vm-champ/corewar' if str(sys.platform).lower() == 'darwin' else './vm-champ-linux/corewar'
-logging.info("path to given corewar executable:%s"%(exe_corewar_sys))
-
-
-# %%
-exe_asm_sys = './mac-os-vm-champ/asm' if str(sys.platform).lower() == 'darwin' else './vm-champ-linux/asm'
-logging.info("path to given asm executable:%s"%(exe_asm_sys))
-
-
-# %%
-exe_corewar_our='./corewar'
-logging.info("path to our corewar executable:%s"%(exe_corewar_our))
-
-
-# %%
-exe_asm_our='./asm'
-logging.info("path to our asm executable:%s"%(exe_asm_our))
-
-
-# %%
-cmd2 = " ".join( [    exe_corewar_sys,					"-d",
-					"0",
-					"mac-os-vm-champ/batman.cor",
-					"mac-os-vm-champ/robin.cor",
-
-])
-stdout_sys = run_cmd(cmd2).replace("\n",":").replace(" ","").split(":")[4::2]
-
-
-# %%
-((stdout_sys))
-
-
-# %%
-cmd1 = " ".join( [    exe_corewar_our,					"-n",
-					"1",
-					"mac-os-vm-champ/batman.cor",
-					"-n",
-					"2",
-					"mac-os-vm-champ/robin.cor",
-
-])
-stdout_our = run_cmd(cmd1).split("\n")
-
-
-# %%
-error = list(stdout_our[10])
-error[7] = '-'
-stdout_our[10] = "".join(error)
-
-
-# %%
 def prinf_diff(string_number,our,sys):
     if our==sys:
         logging.info("Strings number %i equals"%(string_number))
@@ -107,11 +36,93 @@ def prinf_diff(string_number,our,sys):
         logging.warn("sys:%s"%diff_sys)
     return None    
 
+def parse_args(args):
+	res = dict()
+	res['players'] = []
+	i = 1
+	l = len(args)
+	while i < l:
+		if args[i] =='--start':
+			i+=1
+			try:res['start']=int(args[i])
+			except Exception as e: 
+				logging.warn("an error occuring with \'start\' argument: %s"%e)
+				sys.exit(-1)
+		elif args[i] =='--end':
+			i+=1
+			try:res['end']=int(args[i])
+			except Exception as e: 
+				logging.warn("an error occuring with \'end\' argument: %s"%e)
+				sys.exit(-1)
+		else:
+			res['players'].append(args[i])
+		i+=1
+	if 'end' not in res.keys() and 'start' in res.keys():
+		res['end'] = max(res['start'],10)
+	elif 'end' in res.keys() and 'start' not in res.keys():
+		res['start'] = res['end']
+	elif 'end' not in res.keys() and 'start' not in res.keys():
+		res['start'] = 10
+		res['end'] = 10
+	return res
 
-# %%
-[prinf_diff(string_number,our,sys) for string_number,(our, sys) in enumerate(zip(stdout_our,stdout_sys))];
+# 2. Programs definition
 
+logging.info("%s"%(sys.platform))
 
-# %%
+if 'Makefile' in set(os.listdir('.')):
+    logging.info("will call a \'make re\' command")
+    run_cmd("make re")
+else:
+    logging.warn("Makefile abcent, please, add it in dir:%s"%(os.path.abspath('.')))
+
+exe_corewar_sys = './mac-os-vm-champ/corewar' if str(sys.platform).lower() == 'darwin' else './vm-champ-linux/corewar'
+logging.info("path to given corewar executable:%s"%(exe_corewar_sys))
+
+exe_asm_sys = './mac-os-vm-champ/asm' if str(sys.platform).lower() == 'darwin' else './vm-champ-linux/asm'
+logging.info("path to given asm executable:%s"%(exe_asm_sys))
+
+exe_corewar_our='./corewar'
+logging.info("path to our corewar executable:%s"%(exe_corewar_our))
+
+exe_asm_our='./asm'
+logging.info("path to our asm executable:%s"%(exe_asm_our))
+
+# 3. Programs execution
+
+args = parse_args(sys.argv)
+
+logging.info("start at: %i"%(args['start']))
+logging.info("end at: %i"%(args['end']))
+logging.info("players: %s"%(" ".join(args['players'])))
+
+i = args['start']
+players = " ".join(args['players'])
+while i <= args['end']:
+	cmd1 = " ".join([
+				exe_corewar_our,
+				'-dump',
+				str(i),
+				players,
+					])
+
+	stdout_our = run_cmd(cmd1).split("\n")
+
+	cmd2 = " ".join([
+				exe_corewar_sys,
+				'-d',
+				str(i),
+				players,
+	])
+	stdout_sys = run_cmd(cmd2).replace("\n",":").replace(" ","").split(":")[4::2]
+
+	# 4. Compare and print out results 
+
+	[prinf_diff(string_number,our,sys) for string_number,(our, sys) in enumerate(zip(stdout_our,stdout_sys))]
+
+	i+=1
+
+# 5. Final cleanup and finish
+ 
 run_cmd("make fclean")
-
+sys.exit(0)
